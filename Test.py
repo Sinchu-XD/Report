d8d8f0712f1071bf134f225084c3ec3b43cbf5fd
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-22 15:31:15 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -7,5 +7,12 @@ bot = TelegramClient('bot_ss', api_id, api_hash).start(bot_token=BOT_TOKEN)
 async def start(event):
     await event.respond("Hello! Bot is working.")
 
-print("Bot is running...")
-bot.run_until_disconnected()
+async def main():
+    await bot.run_until_disconnected()
+    await asyncio.get_event_loop().create_future()
+
+if __name__ == "__main__":
+    try:
+        asyncio.get_event_loop().run_until_complete(main())
+    except KeyboardInterrupt:
+        print("🛑 Bot stopped manually.")
