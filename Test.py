8c25c68d33665104d6b5ef8524043ec554c12169
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-22 14:55:57 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,12 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import ReportRequest
-from telethon.tl.types import InputReportReasonSpam
+from telethon.tl.types import (InputReportReasonSpam,
+    InputReportReasonViolence,
+    InputReportReasonPornography,
+    InputReportReasonChildAbuse,
+    InputReportReasonCopyright,
+    InputReportReasonFake,
+    InputReportReasonIllegalDrugs,
+    InputReportReasonPersonalDetails,
+    InputReportReasonGeoIrrelevant,
+    InputReportReasonOther)
