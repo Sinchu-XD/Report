7dd108f95265c5206f3294ff0e548003bfa962e4
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-04-22 15:28:18 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, events
 from Report import API_ID as api_id, API_HASH as api_hash, BOT_TOKEN
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=BOT_TOKEN)
+bot = TelegramClient('bot_ss', api_id, api_hash).start(bot_token=BOT_TOKEN)
 
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
